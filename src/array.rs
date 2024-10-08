f0e981c9477bfa84f607568384a06ea8fe892584
sinu <65924192+sinui0@users.noreply.github.com>
2024-10-08 11:30:10 -0700

    feat: add ToBits impl for primitive array

--- src/array.rs
+++ src/array.rs
@@ -1,4 +1,22 @@
-use crate::FromBitIterator;
+use core::slice::Iter as SliceIter;
+
+use crate::{BitLength, FromBitIterator, GetBit, IntoBitIter, Lsb0, Msb0, ToBits};
+
+impl<'a, T, const N: usize> ToBits<'a> for [T; N]
+where
+    &'a T: GetBit<Lsb0> + GetBit<Msb0> + BitLength + 'a,
+{
+    type IterLsb0 = IntoBitIter<SliceIter<'a, T>, Lsb0>;
+    type IterMsb0 = IntoBitIter<SliceIter<'a, T>, Msb0>;
+
+    fn iter_lsb0(&'a self) -> Self::IterLsb0 {
+        IntoBitIter::from(self.iter())
+    }
+
+    fn iter_msb0(&'a self) -> Self::IterMsb0 {
+        IntoBitIter::from(self.iter())
+    }
+}
 
 impl<const N: usize, T> FromBitIterator for [T; N]
 where
